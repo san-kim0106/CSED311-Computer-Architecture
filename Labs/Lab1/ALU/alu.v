b1038c88ba6b3a4e1b23571fc51428401cc0533b
San Kim <sankim@postech.ac.kr>
2023-03-02 20:21:46 +0900

    modified alu.v

--- Labs/Lab1/ALU/alu.v
+++ Labs/Lab1/ALU/alu.v
@@ -1,3 +1,5 @@
+`include "alu_func.v"
+
 module ALU #(parameter data_width = 16) (
 	input [data_width - 1 : 0] A, 
 	input [data_width - 1 : 0] B, 
@@ -8,19 +10,37 @@ module ALU #(parameter data_width = 16) (
 
 // You can declare any variables as needed.
 /*
-	YOUR VARIABLE DECLARATION...
+	
 */
 
 initial begin
 	C = 0;
 	OverflowFlag = 0;
-end   	
+end
 
 // TODO: You should implement the functionality of ALU!
 // (HINT: Use 'always @(...) begin ... end')
-/*
-	YOUR ALU FUNCTIONALITY IMPLEMENTATION...
-*/
+	always @(*) begin
+		case (FuncCode)
+			`FUNC_ADD: C = A + B;
+			`FUNC_SUB: C = A - B;
+			`FUNC_ID: C = A;
+			`FUNC_NOT: C = ~A;
+			`FUNC_AND: C = A & B;
+			`FUNC_OR: C = A | B;
+			`FUNC_NAND: C = ~(A & B);
+			`FUNC_NOR: C = ~(A | B);
+			`FUNC_XOR: C = A ^ B;
+			`FUNC_XNOR: C = ~(A ^ B);
+			`FUNC_LLS: C = A << 1;
+			`FUNC_LRS: C = A >> 1;
+			`FUNC_ALS: C = A <<< 1;
+			`FUNC_ARS: C = A >>> 1;
+			`FUNC_TCP: C = ~A + 1;
+			`FUNC_ZERO: C = 0;
+			default: 
+		endcase
+
+	end
 
 endmodule
-
