82858782cf99350c3094c37529484e65d1ced566
San Kim <86462416+san-kim0106@users.noreply.github.com>
2023-05-30 14:45:25 +0900

    Revert "brought cpu files from Lab4-2 and write some TODOs. added cache module to cpu module"

--- Labs/Lab1/vending_machine/vending_machine_def.v
+++ Labs/Lab1/vending_machine/vending_machine_def.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Title         : vending_machine_def.v
 // Author      : Jae-Eon Jo (Jojaeeon@postech.ac.kr) 
 //					Dongup Kwon (nankdu7@postech.ac.kr)
@@ -13,19 +12,3 @@
 `define kNumCoins 3
 
 `define kWaitTime 100
-=======
-// Title         : vending_machine_def.v
-// Author      : Jae-Eon Jo (Jojaeeon@postech.ac.kr) 
-//					Dongup Kwon (nankdu7@postech.ac.kr)
-
-// Macro constants (prefix k & CamelCase)
-`define kTotalBits 31
-  
-`define kItemBits 8
-`define kNumItems 4
-
-`define kCoinBits 8
-`define kNumCoins 3
-
-`define kWaitTime 100
->>>>>>> 8d624cd1ac5eb7437f38dcdef4afa3f13b2bafec
